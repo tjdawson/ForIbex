32b578268c64408c3a6057a238f5bded7c6ad77d
tjdawson <tjdawson@users.noreply.github.com>
2017-04-06 16:46:00 -0400

    3_24

---
+++ data_includes/Exp3_24.js
@@ -0,0 +1,179 @@
+// A template for the experimental 'data' file for an Ibex experiment
+// NOTE: All the curly braces required for Javascript have been doubled -- this is to ensure compatibility
+// with Python's string.format() method; and so this code will NOT run as is. It is meant to be used
+// within the context of a script that will pass values to replace the pairs of single-brace curly bracket formatting placeholders;
+// which are noted wherever they appear with an in-line comment.
+
+// set send results to manual
+var manualSendResults = true;
+
+var shuffleSequence = seq(
+"intro1", "intro2",
+    // The item sequence goes here
+    "321",
+"1013",
+"211",
+"922",
+"111",
+"421",
+"123",
+"423",
+"1023",
+"913",
+"323",
+"213",
+"112",
+"921",
+"1012",
+"221",
+"311",
+"412",
+"1022",
+"312",
+"212",
+"422",
+"911",
+"121",
+"413",
+"223",
+"113",
+"322",
+"1021",
+"912",
+"222",
+"313",
+"1011",
+"411",
+"122",
+"923",
+ // this is for formatting with python's .format method
+"sr",
+"end"
+);
+
+var defaults = [
+    "Separator", {
+        transfer: "keypress",
+        normalMessage: "Please look up at the display on the wall and watch the video. When it's finished, press any key to start guessing.",
+        errorMessage: "Please wait for the video to finish. Then, press any key to start guessing."
+    },
+    "DashedSentence", {
+        mode: "self-paced reading"
+    },
+    "AcceptabilityJudgment", {
+        as: ["1", "2", "3", "4", "5", "6", "7"],
+        presentAsScale: true,
+        instructions: "Use number keys or click boxes to answer.",
+        leftComment: "(Bad)", rightComment: "(Good)"
+    },
+    "Question", {
+        hasCorrect: true
+    },
+    "Message", {
+        hideProgressBar: true
+    },
+    "Form", {
+        hideProgressBar: false,
+        continueOnReturn: true,
+        saveReactionTime: false
+    }
+];
+
+var items = [
+
+    //
+
+    ["sr", "__SendResults__", {}],
+
+    //
+
+    ["sep", "Separator", { }],
+
+    //
+
+    ["intro", "Form", {
+        html: {include: "intro.html"},
+        hideProgressBar: true,
+        countsForProgressBar: false
+
+    }],
+
+    //
+
+    ["intro1", "Form", {
+        html: {include: "intro1.html"},
+        hideProgressBar: true,
+        countsForProgressBar: false,
+         validators: {
+            age: function (s) { if (s.match(/^\d+$/)) return true; else return "Bad value for \u2018age\u2019"; }
+        }
+
+    }],
+
+    //
+
+    ["intro2", "Form", {
+        html: {include: "intro2.html"},
+        hideProgressBar: true,
+        countsForProgressBar: false
+
+    }],
+
+    //
+
+    ["intro3", "Form", {
+        html: { include: "intro3.html" },
+        hideProgressBar: true,
+        countsForProgressBar: false
+    } ],
+
+    // Here we enter each item as a Form controller pointing to a html file, e.g.:
+    //     ["image_1", "Form", { html: {include: "image_1.html"}}],
+    //     ["image_2", "Form", { html: {include: "image_2.html"}}],
+    //     ...
+    ["321", "Form", { html: {include: "321.html"}}],
+["1013", "Form", { html: {include: "1013.html"}}],
+["211", "Form", { html: {include: "211.html"}}],
+["922", "Form", { html: {include: "922.html"}}],
+["111", "Form", { html: {include: "111.html"}}],
+["421", "Form", { html: {include: "421.html"}}],
+["123", "Form", { html: {include: "123.html"}}],
+["423", "Form", { html: {include: "423.html"}}],
+["1023", "Form", { html: {include: "1023.html"}}],
+["913", "Form", { html: {include: "913.html"}}],
+["323", "Form", { html: {include: "323.html"}}],
+["213", "Form", { html: {include: "213.html"}}],
+["112", "Form", { html: {include: "112.html"}}],
+["921", "Form", { html: {include: "921.html"}}],
+["1012", "Form", { html: {include: "1012.html"}}],
+["221", "Form", { html: {include: "221.html"}}],
+["311", "Form", { html: {include: "311.html"}}],
+["412", "Form", { html: {include: "412.html"}}],
+["1022", "Form", { html: {include: "1022.html"}}],
+["312", "Form", { html: {include: "312.html"}}],
+["212", "Form", { html: {include: "212.html"}}],
+["422", "Form", { html: {include: "422.html"}}],
+["911", "Form", { html: {include: "911.html"}}],
+["121", "Form", { html: {include: "121.html"}}],
+["413", "Form", { html: {include: "413.html"}}],
+["223", "Form", { html: {include: "223.html"}}],
+["113", "Form", { html: {include: "113.html"}}],
+["322", "Form", { html: {include: "322.html"}}],
+["1021", "Form", { html: {include: "1021.html"}}],
+["912", "Form", { html: {include: "912.html"}}],
+["222", "Form", { html: {include: "222.html"}}],
+["313", "Form", { html: {include: "313.html"}}],
+["1011", "Form", { html: {include: "1011.html"}}],
+["411", "Form", { html: {include: "411.html"}}],
+["122", "Form", { html: {include: "122.html"}}],
+["923", "Form", { html: {include: "923.html"}}],
+ // this is for formatting with python's .format method
+
+["end", "Form", {
+        html: {include: "end.html"},
+        hideProgressBar: true,
+        countsForProgressBar: false,
+        continueMessage: null
+
+    }]
+];
